207478d0668beb2be4457bbe83d27c6d50721559
tmddusgood <tmddusgood@gmail.com>
2022-05-11 13:31:59 +0900

    Inflearn Java Algorithm

---
+++ Algorithm/Problems/Others/programmers/level3/파괴되지않은건물.java
@@ -0,0 +1,16 @@
+package programmers.level3;
+
+import java.io.IOException;
+
+public class 파괴되지않은건물 {
+    public static void main(String[] args){
+        int board[][] = {{1,2,3}, {4,5,6}, {7,8,9}};
+        int skill[][] = {{1,1,1,2,2,4},{1,0,0,1,1,2},{2,2,0,2,0,100}};
+        System.out.println(solution(board, skill));
+    }
+
+    public static int solution(int[][] board, int[][] skill){
+        int answer = 0;
+        return answer;
+    }
+}
