c7cab0f756feb498964a7d1b6ab3b5bf89e970ce
tmddusgood <tmddusgood@gmail.com>
2022-09-14 17:33:25 +0900

    acmicpc/stepbystep/function

---
+++ CodingTestPractice/src/acmicpc/stepbystep/function/bj15596.java
@@ -0,0 +1,9 @@
+package acmicpc.stepbystep.function;
+
+public class bj15596 {
+    long sum(int[] a){
+        long r = 0;
+        for (int e = 0; e < a.length; e++) r += a[e];
+        return r;
+    }
+}
