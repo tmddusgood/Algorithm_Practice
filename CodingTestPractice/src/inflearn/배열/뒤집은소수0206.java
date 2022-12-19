1fbddb83a1ef8b01affd33a932d997dea9203981
tmddusgood <tmddusgood@gmail.com>
2022-12-19 17:35:05 +0900

    inflearn/뒤집은소수

--- CodingTestPractice/src/inflearn/배열/뒤집은소수0206.java
+++ CodingTestPractice/src/inflearn/배열/뒤집은소수0206.java
@@ -6,29 +6,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class 뒤집은소수0206 {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            int target = toInt(st.nextToken());
-            if(verifyPrime(target)) sb.append(target).append(" ");
-        }
-
-        System.out.println(sb);
-    }
-
-    static int toInt(String target){
-        StringBuilder sb = new StringBuilder();
-        for (int i = target.length() - 1; i >= 0; i--){
-            sb.append(target.toCharArray()[i]);
-        }
-        return Integer.parseInt(sb.toString());
-    }
-
-    static boolean verifyPrime(int target){
+    public boolean verifyPrime(int target){
         boolean[] noPrime = new boolean[target + 1];
         noPrime[0] = true;
         noPrime[1] = true;
@@ -40,7 +18,30 @@ public class 뒤집은소수0206 {
                 temp++;
             }
         }
-
         return !noPrime[target];
+    }
+    public int toInt(String target){
+        StringBuilder sb = new StringBuilder();
+        for (int i = target.length() - 1; i >= 0; i--){
+            sb.append(target.toCharArray()[i]);
+        }
+        return Integer.parseInt(sb.toString());
+    }
+    public String solution(int N, StringTokenizer st){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            int target = toInt(st.nextToken());
+            if(verifyPrime(target)) sb.append(target).append(" ");
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        뒤집은소수0206 T = new 뒤집은소수0206();
+        System.out.println(T.solution(N, st));
     }
 }
